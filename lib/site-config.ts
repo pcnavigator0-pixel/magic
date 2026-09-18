66c4b7c8692f8c8b82a6233114768bf3f03c5bcb
pcnavigator0 <pcnavigator0@gmail.com>
2026-09-18 17:18:50 +0000

    Set new canonical site domain

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,4 +1,4 @@
-export const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "https://magicbasketball.vercel.app").replace(/\/$/, "");
+export const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "https://www.magicinitiatives.rw").replace(/\/$/, "");
 
 export const siteName = "Magic Basketball Initiatives";
 export const siteAbbreviation = "MBI";
